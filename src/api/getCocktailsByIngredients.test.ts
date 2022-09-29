e915bb70e423ab9096bb6560b6694e1acde38c7f
Olga Urentseva <lelyalelya9666@gmail.com>
2022-09-29 18:28:23 +0200

    add (getCocktailsByIngredients test) "no intersection" case

--- src/api/getCocktailsByIngredients.test.ts
+++ src/api/getCocktailsByIngredients.test.ts
@@ -10,7 +10,7 @@ describe("getCocktailsByIngredients", () => {
     ingredients = ["lime", "vodka", "gin"];
   });
 
-  it("calls API for every ingredient", async () => {
+  it("calls API for every ingredient with correct parameters", async () => {
     const calledIngredients: (string | null)[] = [];
 
     MswServer.use(
@@ -32,7 +32,40 @@ describe("getCocktailsByIngredients", () => {
     expect(calledIngredients).toEqual(ingredients);
   });
 
-  // describe("when there are no intersections", () => {
-  //   it("returns empty array", () => {});
-  // });
+  describe("when there are no intersections", async () => {
+    it("returns empty array", async () => {
+      const mockedResponse = [
+        {
+          drinks: [
+            { strDrink: "Limoncello Spritz", idDrink: "1", strDrinkThumb: "url1" },
+          ],
+        },
+        {
+          drinks: [
+            { strDrink: "White Russian", idDrink: "2", strDrinkThumb: "url2" },
+          ],
+        },
+        {
+          drinks: [
+            {
+              strDrink: "California Lemonade",
+              idDrink: "3",
+              strDrinkThumb: "url3",
+            },
+          ],
+        },
+      ];
+
+      MswServer.use(
+        rest.get(
+          "https://thecocktaildb.com/api/json/v1/1/filter.php",
+          (req, res, ctx) => {
+            return res(ctx.json(mockedResponse));
+          }
+        )
+      );
+    });
+    const cocktails = await getCocktailsByIngredients(ingredients);
+    expect(cocktails).toEqual([]);
+  });
 });
