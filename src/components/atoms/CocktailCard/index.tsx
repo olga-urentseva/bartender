3fcd3adeb9b5ce76a5215f727cb3a3c8ec8cc81a
Olga Urentseva <lelyalelya9666@gmail.com>
2022-10-24 20:53:01 +0200

    fix cocktail links

--- src/components/atoms/CocktailCard/index.tsx
+++ src/components/atoms/CocktailCard/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styled, { DefaultTheme } from "styled-components";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 type CardProps = {
   theme: DefaultTheme;
   picture: string;
 };
 
-const Card = styled.div``;
+const Card = styled.div`
+  position: relative;
+  padding-bottom: 3em;
+`;
 
 const Image = styled.div<CardProps>`
   box-shadow: 0 0.5em 1.5em -0.8em ${(props) => props.theme.accentLighter};
@@ -17,8 +20,8 @@ const Image = styled.div<CardProps>`
   background-size: 100%;
   -webkit-transition: 0.2s ease-in;
   transition: 0.2s ease-in;
-  position: relative;
   padding-bottom: 70%;
+  position: relative;
 
   &:hover {
     cursor: pointer;
@@ -40,13 +43,20 @@ const Image = styled.div<CardProps>`
   }
 `;
 
+const CardLink = styled(Link)`
+  text-decoration: none;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+`;
+
 const CocktailTitle = styled.h2`
   font-size: 1.2em;
   color: ${(props) => props.theme.text};
-`;
-
-const CardLink = styled(Link)`
-  text-decoration: none;
+  position: absolute;
+  bottom: -0.2em;
 `;
 
 type CocktailCardProps = {
@@ -56,14 +66,9 @@ type CocktailCardProps = {
 };
 
 function CocktailCard({ cocktailName, picture, id }: CocktailCardProps) {
-  const navigate = useNavigate();
-  function imageOnClick() {
-    navigate(`cocktails/${id}`);
-  }
-
   return (
     <Card>
-      <Image picture={picture} onClick={imageOnClick} />
+      <Image picture={picture} />
       <CardLink to={`cocktails/${id}`} key={id}>
         <CocktailTitle>{cocktailName}</CocktailTitle>
       </CardLink>
