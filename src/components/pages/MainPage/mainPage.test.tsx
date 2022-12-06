a33b31797a9906c7ebbf511d56b3239aa2187eb7
Olga Urentseva <lelyalelya9666@gmail.com>
2022-12-05 22:05:57 -0800

    feat(Loader): role for loader

--- src/components/pages/MainPage/mainPage.test.tsx
+++ src/components/pages/MainPage/mainPage.test.tsx
@@ -1,4 +1,4 @@
-import { act, render, screen } from "@testing-library/react";
+import { act, render, screen, fireEvent } from "@testing-library/react";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
 import MainPage from "./index";
@@ -26,16 +26,49 @@ describe("MainPage", () => {
     );
   }
 
-  describe("successfully loaded with NO cocktails", () => {
-    let p: Promise<any> | undefined;
-    mockedLoader = vi.fn(() => {
-      const promise = Promise.resolve([]);
-      p = promise;
-      return promise;
-    });
+  describe("error", () => {
+    let p: Promise<unknown> | undefined;
 
     beforeEach(() => {
       p = undefined;
+      mockedLoader = vi.fn(() => {
+        console.log("this is mocked loader inside before each");
+
+        const promise = Promise.reject(new Error());
+        p = promise;
+        return promise;
+      });
+    });
+
+    it("shows error page", async () => {
+      render(<RouterProvider router={getMockedRouter()} />);
+
+      await act(async () => {
+        // we should wait untill promise resolve, because only then react will rerender the page with expected message
+
+        try {
+          await p;
+        } catch (error) {
+          // i had to write this comment because if catch is empty I'll get the error and component won't be rendered
+        }
+      });
+
+      expect(
+        screen.getByText("🗿 Error: Something went wrong 🗿")
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe("successfully loaded with NO cocktails", () => {
+    let p: Promise<any> | undefined;
+
+    beforeEach(() => {
+      p = undefined;
+      mockedLoader = vi.fn(() => {
+        const promise = Promise.resolve([]);
+        p = promise;
+        return promise;
+      });
     });
 
     it("should show 'no cocktails' message", async () => {
@@ -52,5 +85,5 @@ describe("MainPage", () => {
       ).toBeInTheDocument();
     });
   });
-  describe("successfully loaded with SOME cocktails", () => {});
+  // describe("successfully loaded with SOME cocktails", () => {});
 });
