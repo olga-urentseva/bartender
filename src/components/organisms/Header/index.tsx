148616c20cde0e1ff1c60a4f01e22d10e7b5f968
Olga Urentseva <lelyalelya9666@gmail.com>
2022-12-09 16:33:22 -0800

    feat(Header): link to cocktail library

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -20,10 +20,17 @@ const HeaderWrapper = styled.div`
   margin: 0;
 `;
 
+const InnerWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const LogoLink = styled(Link)`
   text-decoration: none;
   padding: 1em;
   margin-left: -1em;
+  display: inline-block;
 `;
 
 const Logo = styled.h2`
@@ -35,14 +42,30 @@ const Logo = styled.h2`
   display: inline-block;
 `;
 
+const MenuLink = styled(Link)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  font-weight: 600;
+  display: inline-block;
+
+  &:hover,
+  &:active,
+  &:focus-within {
+    color: ${(props) => props.theme.textLighter};
+  }
+`;
+
 export default function Header() {
   return (
     <HeaderComponent>
       <HeaderWrapper>
         <Container>
-          <LogoLink to={"/"}>
-            <Logo>Bart-t-tender</Logo>
-          </LogoLink>
+          <InnerWrapper>
+            <LogoLink to={"/"}>
+              <Logo>Bart-t-tender</Logo>
+            </LogoLink>
+            <MenuLink to={"/cocktails"}>Cocktail Library</MenuLink>
+          </InnerWrapper>
         </Container>
       </HeaderWrapper>
     </HeaderComponent>
