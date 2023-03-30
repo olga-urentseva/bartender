77875cc27238cbaa6e0c93814691ade96d8b7be5
Olga Urentseva <lelyalelya9666@gmail.com>
2023-03-29 23:03:57 -0700

    feat(Search form component): search button

--- src/components/organisms/SearchForm/index.tsx
+++ src/components/organisms/SearchForm/index.tsx
@@ -1,7 +1,8 @@
-import React, { FormEvent } from "react";
+import { FormEvent } from "react";
 import styled from "styled-components";
 
 import Input from "../../atoms/Input";
+import SearchIcon from "./SearchIcon";
 
 const Form = styled.form`
   display: flex;
@@ -11,13 +12,36 @@ const Form = styled.form`
   flex: 3 1 auto;
 `;
 
+const InnerWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+`;
+
 const Label = styled.label`
   font-size: 1.5em;
   color: ${(props) => props.theme.accent};
   font-weight: 500;
 `;
 
-const SearchButton = styled.button``;
+const SearchButton = styled.button`
+  background-color: transparent;
+  border: none;
+  position: absolute;
+  right: 0;
+  top: 30%;
+  padding: 0 1em;
+
+  &:hover,
+  &:focus,
+  &:active {
+    cursor: pointer;
+
+    ${SearchIcon} {
+      stroke: ${({ theme }) => theme.accentLighter};
+    }
+  }
+`;
 
 type SearchFormProps = {
   title: string;
@@ -41,14 +65,18 @@ function SearchForm({ title, onFormSubmit, items }: SearchFormProps) {
   return (
     <Form onSubmit={onSubmit}>
       <Label>{title}</Label>
-      <Input
-        key={items}
-        type="text"
-        placeholder="Lime"
-        name="search"
-        defaultValue={items}
-      />
-      <SearchButton type="submit">Search</SearchButton>
+      <InnerWrapper>
+        <Input
+          key={items}
+          type="text"
+          placeholder="Lime"
+          name="search"
+          defaultValue={items}
+        />
+        <SearchButton type="submit" aria-label="Search">
+          <SearchIcon color={""} />
+        </SearchButton>
+      </InnerWrapper>
     </Form>
   );
 }
