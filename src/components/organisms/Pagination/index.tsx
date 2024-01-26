24cc357fb7cd51a89b7309de942b64b5064e0c1a
Olga Urentseva <lelyalelya9666@gmail.com>
2024-01-25 16:35:24 -0800

    pagination fix

--- src/components/organisms/Pagination/index.tsx
+++ src/components/organisms/Pagination/index.tsx
@@ -26,10 +26,10 @@ const PaginationInfoWrapper = styled.div`
 
 const DirectionButton = styled.button`
   background-color: ${(props) => props.theme.accentPastel};
-  font-size: 1.2em;
+  font-size: 1em;
   border: none;
   border-radius: 1em;
-  padding: 0.5em 1em;
+  padding: 0.2em 0.5em;
   color: ${(props) => props.theme.text};
   transition: 0.3s;
   font-weight: 600;
@@ -48,7 +48,9 @@ const DirectionButton = styled.button`
 `;
 
 const PageButton = styled.button<{ isHighlighted: boolean }>`
-  width: 2rem;
+  width: 1rem;
+  margin: 0;
+  padding: 0;
   border: none;
   background-color: transparent;
   font-size: 1rem;
@@ -71,46 +73,68 @@ export default function Pagination(props: PaginationProps) {
     const MAX_PAGES = 3;
     const buttons: ReactElement[] = [];
 
-    if (pagesNumber <= MAX_PAGES) {
-      for (let pageNumber = 1; pageNumber <= pagesNumber; pageNumber++) {
-        buttons.push(
-          <PageButton
-            onClick={() => props.setPageNumber(pageNumber)}
-            key={pageNumber}
-            isHighlighted={props.currentPageNumber === pageNumber}
-          >
-            {pageNumber}
-          </PageButton>
-        );
-      }
-    } else {
-      for (let pageNumber = 1; pageNumber <= MAX_PAGES; pageNumber++) {
-        buttons.push(
-          <PageButton
-            onClick={() => {
-              return props.setPageNumber(pageNumber);
-            }}
-            key={pageNumber}
-            isHighlighted={props.currentPageNumber === pageNumber}
-          >
-            {pageNumber}
-          </PageButton>
-        );
-      }
-      buttons.push(<span key="ellipsis">...</span>);
+    const addPageButton = (pageNumber: number) => {
       buttons.push(
         <PageButton
-          onClick={() => props.setPageNumber(pagesNumber)}
-          key={pagesNumber}
-          isHighlighted={props.currentPageNumber === pagesNumber}
+          onClick={() => props.setPageNumber(pageNumber)}
+          key={pageNumber}
+          isHighlighted={props.currentPageNumber === pageNumber}
         >
-          {pagesNumber}
+          {pageNumber}
         </PageButton>
       );
+    };
+
+    const addEllipsis = (key: string) => {
+      buttons.push(<span key={key}>...</span>);
+    };
+
+    if (pagesNumber <= MAX_PAGES) {
+      for (let pageNumber = 1; pageNumber <= pagesNumber; pageNumber++) {
+        addPageButton(pageNumber);
+      }
+    } else {
+      const current = props.currentPageNumber;
+      const last = pagesNumber;
+
+      if (current <= MAX_PAGES - 1) {
+        for (let pageNumber = 1; pageNumber <= MAX_PAGES; pageNumber++) {
+          addPageButton(pageNumber);
+        }
+        addEllipsis("ellipsis1");
+        addPageButton(last);
+      } else if (current >= last - MAX_PAGES + 2) {
+        addPageButton(1);
+        addEllipsis("ellipsis2");
+        for (
+          let pageNumber = last - MAX_PAGES + 1;
+          pageNumber <= last;
+          pageNumber++
+        ) {
+          addPageButton(pageNumber);
+        }
+      } else {
+        console.log("lal");
+
+        addPageButton(1);
+        if (current !== MAX_PAGES) {
+          addEllipsis("ellipsis3");
+        }
+        const startPage = Math.max(2, current - 1);
+        const endPage = Math.min(last - 1, current + 1);
+
+        for (let pageNumber = startPage; pageNumber <= endPage; pageNumber++) {
+          addPageButton(pageNumber);
+        }
+        if (current !== last - MAX_PAGES + 1) {
+          addEllipsis("ellipsis4");
+        }
+        addPageButton(last);
+      }
     }
+
     return buttons;
   }
-
   return (
     <PaginationWrapper disabled={props.isDisabled}>
       <DirectionButton
