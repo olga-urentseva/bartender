72b120c4799a452a94ae5ff080453c439c8299bd
Olga Urentseva <o.urentseva@gmail.com>
2026-02-23 15:46:19 -0800

    try to fix conditional imports of css files

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
 
 const savedTheme = localStorage.getItem("theme");
 
-if (savedTheme === "spring") {
-  import("./index-spring.css");
-} else {
-  import("./index.css");
-}
+const cssLoad = savedTheme === "spring"
+  ? import("./index-spring.css")
+  : import("./index.css");
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+cssLoad.then(() => {
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
