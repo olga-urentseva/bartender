f37df4406a8c8f7fc23d956f41c6cc4fb0c8ede2
Olga Urentseva <o.urentseva@gmail.com>
2026-04-01 16:59:59 -0700

    upgrade to react 19

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,9 +1,8 @@
 import "styled-components";
-import { themeNew } from "./styles/theme";
+import { theme } from "./styles/theme";
 
-type MyTheme = typeof themeNew;
+type MyTheme = typeof theme;
 
 declare module "styled-components" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends MyTheme {}
 }
